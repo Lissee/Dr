a18ed1b50d1687e5773fcfbaa6c55da704bacf69
Lissee <kositcyna.am@students.dvfu.ru>
2020-10-11 21:04:45 +1000

    Commit9

--- main.cpp
+++ main.cpp
@@ -4,41 +4,46 @@
 using namespace std;
 
 int main() {
-    int X, Y, Result =0;
+    int X, Y, Result = 0, Check;
     double R;
-   cout << "Введите координаты X и Y"<<endl;
-   cin >> X;
-    //Проверка введенных данных
-    if (X>5 || X<-5) {
-        cout <<"Ошибка! Введенные координаты не соответствуют допустимым!";
-    } else {
-        cin >> Y;
-        if (Y > 5 || Y < -5) {
-            cout << "Ошибка! Введенные координаты не соответствуют допустимым!";
+    bool check_k = true;
+
+//Стреляем
+    for (Check = 0; Check<5; Check++ ) {
+        cout << "Введите координаты X и Y (Не больше 10 и не меньше -10)" << endl;
+        cin >> X;
+        //Проверка введенных данных
+        if (X>5 || X<-5) {
+            check_k = false;
+            cout <<"Ошибка! Введенные координаты не соответствуют допустимым!";
+            break;
         } else {
-            //Находим радиус мишени
-            X = X + rand() % 5 + (-1);
-            Y = Y + rand() % 5 + (-1);
-            R = sqrt(X * X + Y * Y);
-            cout << "X=" << X << " Y=" << Y << " R=" << R;
-            //Считаем колличество баллов
-            if (R > 5) {
-            } else if (R > 4) {
-                Result = Result + 1;
-            } else if (R > 3) {
-                Result = Result + 2;
-            } else if (R > 2) {
-                Result = Result + 3;
-            } else if (R > 1) {
-                Result = Result + 4;
-            } else Result = Result + 5;
-            cout << "Result: " << Result << endl;
-
-
+            cin >> Y;
+            if (Y > 5 || Y < -5) {
+                check_k = false;
+                cout << "Ошибка! Введенные координаты не соответствуют допустимым!";
+                break;
+            } else {
+                //Находим радиус мишени
+                X = X + rand() % 5 + (-1);
+                Y = Y + rand() % 5 + (-1);
+                R = sqrt(X * X + Y * Y);
+//Считаем колличество баллов
+                if (R > 5) {
+                } else if (R > 4) {
+                    Result = Result + 1;
+                } else if (R > 3) {
+                    Result = Result + 2;
+                } else if (R > 2) {
+                    Result = Result + 3;
+                } else if (R > 1) {
+                    Result = Result + 4;
+                } else Result = Result + 5;
+                cout << "Result: " << Result << endl;
+            }
 //Выводим результат
-cout << "Result: " << Result;
-
-}
+            cout << "Result: " << Result;
+        }
     }
     return 0;
 
