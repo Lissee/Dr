3d89634dc76be40cb041712bcb26023165f121ca
Lissee <kositcyna.am@students.dvfu.ru>
2020-10-11 20:45:09 +1000

    Commit3

--- main.cpp
+++ main.cpp
@@ -5,12 +5,25 @@ using namespace std;
 
 int main() {
     int X, Y;
+    bool check_k;
     double R;
    cout << "Введите координаты X и Y"<<endl;
    cin >> X;
-   cin >> Y;
-   R=sqrt(X*X + Y*Y);
-   cout <<"X="<<X<<" Y="<<Y<<" R="<<R;
+    //Проверка введенных данных
+    if (X>5 || X<-5) {
+        check_k = false;
+        cout <<"Ошибка! Введенные координаты не соответствуют допустимым!";
+    } else {
+        cin >> Y;
+        if (Y > 5 || Y < -5) {
+            check_k = false;
+            cout << "Ошибка! Введенные координаты не соответствуют допустимым!";
+        } else {
+            //Находим радиус мишени
+            R = sqrt(X * X + Y * Y);
+            cout << "X=" << X << " Y=" << Y << " R=" << R;
+        }
+    }
     return 0;
 
 }
